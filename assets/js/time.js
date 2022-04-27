ab72082954ef8fe5699058c8a8d5d06a84a8f92e
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-04-27 16:17:22 -0400

    time api placement

--- assets/js/time.js
+++ assets/js/time.js
@@ -12,14 +12,8 @@ if (year < 1000) {
 var monthArray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
 
 var dayArray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-function timeApi() {
-    var timeApiEl = document.querySelector("#time-api");
-    var timeDisplayEl = document.createElement("div");
-    timeDisplayEl.className = "time-api-display";
-    timeDisplayEl.innerHTML = "<p>" + dayArray[day] + ", " + monthArray[month] + " " + date + ", " + year + "</p>";
-    timeApiEl.appendChild(timeDisplayEl);
-}
-document.write(dayArray[day] + ", " + monthArray[month] + " " + date + ", " + year)
 
+
+document.getElementById("time-api").innerHTML = "<p>" + dayArray[day] + ", " + monthArray[month] + " " + date + ", " + year + "</p>";
 
 httpGetAsync(url)
